24648de8ffa2293874cc873dbf0ad7e51dacea1e
sabdulass <48061544+sabdulass@users.noreply.github.com>
2019-03-01 14:35:50 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("550992032793886740")
+let channel =     client.channels.get("550677148705357826")
 setInterval(function() {
 channel.send(`fughdfgıudhfgıudsfhgısudfhgslghsfgıhsdfıguhsdfgıudfhgısufraer0tuefdflbhdfrg904534ıfhvgurfghwadfkwepotu3brniytreuperuteptertert`);
 }, 30)
